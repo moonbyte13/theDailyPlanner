72db125b635be981db005d365d61a061eedcc4ac
Michael <fraynemichael300@gmail.com>
2023-05-06 00:22:20 -0600

    added starter code for journal

---
+++ client/src/components/JournalWidget/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const JournalWidget = () => {
+  return (
+    <div className="journal-widget">
+      <h3>Journal</h3>
+      {/* Add your journal implementation here */}
+    </div>
+  );
+};
+
+export default JournalWidget;
