9e9bacdaa218f5108aab70ac96be5f8c9ccc7cd9
Michael <fraynemichael300@gmail.com>
2023-05-06 00:21:26 -0600

    added starter code for self-reflection

---
+++ client/src/components/SelfReflectionWidget/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const SelfReflectionWidget = () => {
+  return (
+    <div className="self-reflection-widget">
+      <h3>Self-Reflection</h3>
+      {/* Add your self-reflection implementation here */}
+    </div>
+  );
+};
+
+export default SelfReflectionWidget;
