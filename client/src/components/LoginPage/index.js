b905ac73eec1e9ad08313185bcbbd4ff2e75924c
Michael <fraynemichael300@gmail.com>
2023-05-06 03:17:22 -0600

    got login page to be visible

--- client/src/components/LoginPage/index.js
+++ client/src/components/LoginPage/index.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Input, Label } from '@unojsx/react';
-import { Button } from '@unojsx/react-tailwind';
 
 function LoginPage() {
   const [email, setEmail] = useState('');
@@ -18,10 +16,10 @@ function LoginPage() {
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <form className="bg-white p-6 rounded-lg shadow-lg" onSubmit={handleLogin}>
         <div className="mb-4">
-          <Label htmlFor="email" className="block mb-1 font-medium text-gray-700">
+          <label htmlFor="email" className="block mb-1 font-medium text-gray-700">
             Email:
-          </Label>
-          <Input
+          </label>
+          <input
             id="email"
             type="email"
             value={email}
@@ -32,10 +30,10 @@ function LoginPage() {
           />
         </div>
         <div className="mb-6">
-          <Label htmlFor="password" className="block mb-1 font-medium text-gray-700">
+          <label htmlFor="password" className="block mb-1 font-medium text-gray-700">
             Password:
-          </Label>
-          <Input
+          </label>
+          <input
             id="password"
             type="password"
             value={password}
@@ -46,9 +44,9 @@ function LoginPage() {
           />
         </div>
         <div className="mb-6">
-          <Button type="submit" className="w-full py-2 px-4 rounded-lg text-white bg-blue-500 hover:bg-blue-600">
+          <button type="submit" className="w-full py-2 px-4 rounded-lg text-white bg-blue-500 hover:bg-blue-600">
             Login
-          </Button>
+          </button>
         </div>
       </form>
     </div>
